3ea447aeb5dfa13469628c80bcc4dc1ba266d1c7
John Torres <apodpm@gmail.com>
2023-07-13 00:12:37 +0300

    🐛 fix(shuffle.ts): update regex pattern in keepChars function to include Chinese characters 🔥 chore(shuffle.ts): remove commented out code for newFile variable

--- shuffle.ts
+++ shuffle.ts
@@ -3,7 +3,7 @@ import path from 'path';
 import { getMp3Files, askForPath, readTags } from './fn';
 
 const keepChars = function (str: string = ''): string {
-  return str.replace(`'s`, 's').replace(/[^a-zA-Z0-9]/g, ' ').trim().replace(/\s+/g, '_');
+  return str.replace(`'s`, 's').replace(/[^\w\d\u4e00-\u9fa5]/g, ' ').trim().replace(/\s+/g, '_');
 }
 
 const main = async function () {
@@ -21,7 +21,6 @@ const main = async function () {
     const tags = readTags(p2f);
     const newFile = `${String(shuffle.pop()).padStart(padLength, '0')} - ${keepChars(tags.title)}.mp3`;
     // const newFile = `${String(shuffle.pop()).padStart(padLength, '0')} - ${keepChars(tags.title)} - ${keepChars(tags.artist)}.mp3`;
-
     fs.renameSync(p2f, path.join(dir, newFile));
     console.log(i + 1, newFile);
   });
